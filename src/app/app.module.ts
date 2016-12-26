601a3d7796f9850e7ec339bab8ba2b87b278b39f
Brian Burlingame <brian.burlingame@hotmail.com>
2016-12-26 08:35:40 -0800

    Added 2 colum edit layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { TypeaheadModule  } from 'ng2-bootstrap';
 import { SmartInput } from './ui/smartinput';
+import { SmartInput } from './ui/smartinput/smartinput';
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
 import { PersonListComponent } from './person/personlist.component';
--- src/app/person/person.service.ts
+++ src/app/person/person.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Person } from '../person/person';
+import { Address } from '../address/address';
 
 @Injectable()
 export class PersonService
 {
     getPersons(): Person[]{
         return [
-            { Id: 1, FirstName: 'Brian', LastName: 'Burlingame'},
-            { Id: 2, FirstName: 'Madeline', LastName: 'Burlingame'}
+            { Id: 1, FirstName: 'Brian', LastName: 'Burlingame', 
+                PrimaryAddress: {StreetAddress1:"1234"}
+            },
+            { Id: 2, FirstName: 'Madeline', LastName: 'Burlingame', 
+                PrimaryAddress: {StreetAddress1:"5678"}
+            }
         ];
     }
 }
