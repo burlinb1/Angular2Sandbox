21b5df9a7da113ed21ba57aa8dc32d6db57c63fe
Brian Burlingame <brian.burlingame@hotmail.com>
2017-01-05 19:55:41 -0800

    Added jquery.js for bootstrap requirements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 //import 'bootstrap/dist/js/bootstrap.min';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {  
-
+    toggleDropdown(event){
+      //this.value = this.value + event.key.toUpperCase();
+      //event.preventDefault(); 
+      //alert('click');       
+    }    
 }
